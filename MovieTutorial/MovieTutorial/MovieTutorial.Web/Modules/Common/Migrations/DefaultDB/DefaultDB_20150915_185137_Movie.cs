aba0dcbd86876043b34d89e706a9231b175b2e7c
volkan.ceylan <volkanceylan@gmail.com>
2015-09-20 11:51:52 +0300

    created movie table with a migration

---
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20150915_185137_Movie.cs
@@ -0,0 +1,27 @@
+using FluentMigrator;
+using System;
+
+namespace MovieTutorial.Migrations.DefaultDB
+{
+    [Migration(20150915185137)]
+    public class DefaultDB_20150915_185137_Movie : Migration
+    {
+        public override void Up()
+        {
+            Create.Schema("mov");
+
+            Create.Table("Movie").InSchema("mov")
+                .WithColumn("MovieId").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("Title").AsString(200).NotNullable()
+                .WithColumn("Description").AsString(1000).Nullable()
+                .WithColumn("Storyline").AsString(Int32.MaxValue).Nullable()
+                .WithColumn("Year").AsInt32().Nullable()
+                .WithColumn("ReleaseDate").AsDateTime().Nullable()
+                .WithColumn("Runtime").AsInt32().Nullable();
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
