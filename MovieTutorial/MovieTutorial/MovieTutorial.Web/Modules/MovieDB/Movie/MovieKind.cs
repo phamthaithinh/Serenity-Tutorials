20bd09349dbec7eb778d1058152fdea6debfd079
volkan.ceylan <volkanceylan@gmail.com>
2015-09-24 14:31:46 +0300

    added MovieKind enumeration

---
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Movie/MovieKind.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel;
+
+namespace MovieTutorial.MovieDB
+{
+    public enum MovieKind
+    {
+        [Description("Film")]
+        Film = 1,
+        [Description("TV Series")]
+        TvSeries = 2,
+        [Description("Mini Series")]
+        MiniSeries = 3
+    }
+}
