e7a629f0a3cae3175e5e231bc1908cfe0cdd0620
volkan.ceylan <volkanceylan@gmail.com>
2015-09-24 15:45:06 +0300

    added migration for sample genres

---
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20150924_154100_SampleGenres.cs
@@ -0,0 +1,42 @@
+using FluentMigrator;
+using System;
+
+namespace MovieTutorial.Migrations.DefaultDB
+{
+    [Migration(20150924154100)]
+    public class DefaultDB_20150924_154100_SampleGenres : Migration
+    {
+        public override void Up()
+        {
+            Insert.IntoTable("Genre").InSchema("mov")
+                .Row(new
+                {
+                    Name = "Action"
+                })
+                .Row(new
+                {
+                    Name = "Drama"
+                })
+                .Row(new
+                {
+                    Name = "Comedy"
+                })
+                .Row(new
+                {
+                    Name = "Sci-fi"
+                })
+                .Row(new
+                {
+                    Name = "Fantasy"
+                })
+                .Row(new
+                {
+                    Name = "Documentary"
+                });
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
