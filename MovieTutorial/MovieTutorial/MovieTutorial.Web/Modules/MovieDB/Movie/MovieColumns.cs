fe7e1880b46cb8eaabcd37a760089fcfd098ccda
volkan.ceylan <volkanceylan@gmail.com>
2015-09-25 17:13:17 +0300

    shown GenreName in the movie grid

--- MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Movie/MovieColumns.cs
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Movie/MovieColumns.cs
@@ -21,6 +21,8 @@ namespace MovieTutorial.MovieDB.Forms
         public String Storyline { get; set; }
         public Int32 Year { get; set; }
         public DateTime ReleaseDate { get; set; }
+        [Width(100)]
+        public String GenreName { get; set; }
         [DisplayName("Runtime in Minutes"), Width(150), AlignRight]
         public Int32 Runtime { get; set; }
     }
