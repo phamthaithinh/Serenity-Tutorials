5954492da201cc389ade6afff6c4263a21d21a59
volkan.ceylan <volkanceylan@gmail.com>
2015-10-26 23:18:10 +0200

    edited MovieCastForm

--- MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/MovieDB/MovieCast/MovieCastForm.cs
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/MovieDB/MovieCast/MovieCastForm.cs
@@ -1,19 +1,14 @@
 ﻿
 namespace MovieTutorial.MovieDB.Forms
 {
-    using Serenity;
     using Serenity.ComponentModel;
-    using Serenity.Data;
     using System;
-    using System.ComponentModel;
-    using System.Collections.Generic;
-    using System.IO;
 
     [FormScript("MovieDB.MovieCast")]
     [BasedOnRow(typeof(Entities.MovieCastRow))]
     public class MovieCastForm
     {
-        public Int32 MovieId { get; set; }
+        [LookupEditor(typeof(Entities.PersonRow))]
         public Int32 PersonId { get; set; }
         public String Character { get; set; }
     }
