c8e414c9e121357889061f6a837e61328ba7f942
volkan.ceylan <volkanceylan@gmail.com>
2015-10-26 22:06:19 +0200

    defined MovieCastEditor

---
+++ MovieTutorial/MovieTutorial/MovieTutorial.Script/MovieDB/MovieCast/MovieCastEditor.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MovieTutorial.MovieDB
+{
+    using Common;
+    using jQueryApi;
+    using Serenity;
+    using System.Linq;
+
+    [ColumnsKey("MovieDB.MovieCast"), LocalTextPrefix("MovieDB.MovieCast")]
+    public class MovieCastEditor : GridEditorBase<MovieCastRow>
+    {
+        public MovieCastEditor(jQueryObject container)
+            : base(container)
+        {
+        }
+    }
+}
