7233e9c421d653880f28b7161e6588c1c866bb45
volkan.ceylan <volkanceylan@gmail.com>
2015-10-26 22:12:42 +0200

    re-run t4 templates

--- MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/Common/Imports/ScriptEditorTypes.cs
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/Common/Imports/ScriptEditorTypes.cs
@@ -52,6 +52,19 @@ namespace MovieTutorial.Common
     }
 }
 
+namespace MovieTutorial.MovieDB
+{
+    public partial class MovieCastEditorAttribute : CustomEditorAttribute
+    {
+        public const string Key = "MovieTutorial.MovieDB.MovieCastEditor";
+    
+        public MovieCastEditorAttribute()
+            : base(Key)
+        {
+        }
+    }
+}
+
 namespace MovieTutorial.Northwind
 {
     public partial class PhoneEditorAttribute : CustomEditorAttribute
